a4b03ff8b87d2e2de0eccb2d22927c4c3068a32c
Erkan BÜKER <eerkanbuker@gmail.com>
2021-01-20 22:01:31 +0300

    Homework loaded!

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " customer added!");
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine(customer.Name + " " + customer.Surname + " customer deleted!");
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Name\t: " + customer.Name 
+                + "\nSurname\t: " + customer.Surname 
+                + "\nSalary\t: " + customer.Salary.ToString()
+                + "\ncustomer listed!");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            Customer customer1 = new Customer();
+            customer1.ID = 1;
+            customer1.Name = "Erkan";
+            customer1.Surname = "BUKER";
+            customer1.Salary = 1000;
+
+            customerManager.Add(customer1);
+            customerManager.Delete(customer1);
+            customerManager.List(customer1);
+        }
+    }
+}
